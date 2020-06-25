8bd9b655ef44b676d6a29972a6a399ba09627cc0
JumarySanchez <sanchezjumary@gmail.com>
2020-06-25 11:11:40 -0400

    firstFour

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+function callChallengeBanner(num){
+    num=num+1
+    console.log("######"+ " " + "challenge" + num + " " + "######")
+}
+callChallengeBanner(0)
+
+
 function getVowels(str){
     // let i = str[i]
     let i= 0
@@ -21,7 +28,7 @@ function firstFive(str){
     let i=0
 
     while(i<str.length){
-        if(vowels.includes(str[i])&& count <5){
+        if(vowels.includes(str[i]) && count <5){
             console.log(str[i])
             count=count +1
         }
@@ -29,5 +36,26 @@ function firstFive(str){
     }
 }
 
-firstFive()
+firstFive("Hello!")
 callChallengeBanner(2)
+
+
+function everyThird(str){
+let i = 2
+while(i <str.length){
+    console.log(str[i])
+ i = i +3
+} 
+}
+everyThird("I am the alfalfa and the omelette.")
+callChallengeBanner(3)
+
+
+function firstFourChars(str){
+    let i = 0
+    while(i<str.length && i<4){
+        console.log(str[i])
+        i++ 
+    }
+}
+firstFourChars()
